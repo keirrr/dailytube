a82413c2f087de17a6721e4d60a7737abc3f6ad1
keirrr <hadekshd@gmail.com>
2022-03-05 12:42:12 +0100

    Show interface after login

--- lib/screens/nav_screen.dart
+++ lib/screens/nav_screen.dart
@@ -5,8 +5,10 @@ import './movies_screen.dart';
 import 'upload_video_screen.dart';
 import './categories_screen.dart';
 import './profile_account.dart';
+import './profile_login_screen.dart';
 
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import '../bartek_color_palette.dart';
 
@@ -28,59 +30,66 @@ class _NavScreenState extends State<NavScreen> {
   ];
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: _screens
-            .asMap()
-            .map((i, screen) => MapEntry(
-                  i,
-                  Offstage(
-                    offstage: _selectedIndex != i,
-                    child: screen,
-                  ),
-                ))
-            .values
-            .toList(),
-      ),
-      extendBody: true,
-      backgroundColor: BartekColorPalette.bartekGrey[900],
-      bottomNavigationBar: ClipRRect(
-        borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(15.0),
-          topRight: Radius.circular(15.0),
-        ),
-        child: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: BartekColorPalette.bartekGrey[400],
-          selectedItemColor: Theme.of(context).colorScheme.secondary,
-          unselectedItemColor: BartekColorPalette.bartekGrey[200],
-          currentIndex: _selectedIndex,
-          onTap: (i) => setState(() => _selectedIndex = i),
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Główna',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.movie),
-              label: 'Filmy',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.upload),
-              label: 'Prześlij',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.space_dashboard),
-              label: 'Kategorie',
-            ),
-            BottomNavigationBarItem(
-              icon: FaIcon(FontAwesomeIcons.solidUser),
-              label: 'Profil',
-            )
-          ],
-        ),
-      ),
-    );
-  }
+  Widget build(BuildContext context) => StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Scaffold(
+              body: Stack(
+                children: _screens
+                    .asMap()
+                    .map((i, screen) => MapEntry(
+                          i,
+                          Offstage(
+                            offstage: _selectedIndex != i,
+                            child: screen,
+                          ),
+                        ))
+                    .values
+                    .toList(),
+              ),
+              extendBody: true,
+              backgroundColor: BartekColorPalette.bartekGrey[900],
+              bottomNavigationBar: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(15.0),
+                  topRight: Radius.circular(15.0),
+                ),
+                child: BottomNavigationBar(
+                  type: BottomNavigationBarType.fixed,
+                  backgroundColor: BartekColorPalette.bartekGrey[400],
+                  selectedItemColor: Theme.of(context).colorScheme.secondary,
+                  unselectedItemColor: BartekColorPalette.bartekGrey[200],
+                  currentIndex: _selectedIndex,
+                  onTap: (i) => setState(() => _selectedIndex = i),
+                  items: const <BottomNavigationBarItem>[
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.home),
+                      label: 'Główna',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.movie),
+                      label: 'Filmy',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.upload),
+                      label: 'Prześlij',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.space_dashboard),
+                      label: 'Kategorie',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: FaIcon(FontAwesomeIcons.solidUser),
+                      label: 'Profil',
+                    )
+                  ],
+                ),
+              ),
+            );
+          } else {
+            return ProfileLogin();
+          }
+        },
+      );
 }
